a37af5f7290ea221a423111f6a63a0249e087f64
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-24 06:54:06 -0800

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[5],num,i,k;
+scanf("%d",&num);
+for(i=0;i<num;i++)
+{
+scanf("%d",&a[i]);
+}
+k=0;
+for(i=0;i<num;i++)
+{
+if(a[i]>k)
+{
+k=a[i];
+}
+}
+printf("%d",k);
+return 0;
+}
