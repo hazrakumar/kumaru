95bc02f70acc9e153a94a5dad3dd804b231a7791
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-25 11:27:04 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,11 @@
+    #include<stdio.h>
+    int main()
+    {
+    int hr1,min1,hr2,min2,sub1,sub2;
+    scanf("%d\t%d",&hr1,&min1);
+    scanf("%d\t%d",&hr2,&min2);
+    sub1=hr1-hr2;
+    sub2=min1-min2;
+    printf("%d\t%d",sub1,sub2);
+    return 0;
+    }
