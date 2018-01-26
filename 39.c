9ce5f22f5c36142136fe9a19f1b35176ed0b7f5e
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-26 06:28:29 -0800

    Update 39.c

--- 39.c
+++ 39.c
@@ -4,15 +4,18 @@ int main()
 int a[10],i,j,temp;
 for(i=0;i<10;i++)
 {
-scanf("%d",a[i]);
+scanf("%d",&a[i]);
 }
 for(i=0;i<10;i++)
 {
 for(j=i+1;j<10;j++)
 {
+if(a[i]>a[j])
+{
 temp=a[i];
 a[i]=a[j];
 a[j]=temp;
+}
 }
 }
 printf("%d",a[9]);
