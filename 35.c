09af961ad88535881433650f3f202d010cfab955
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-26 14:24:26 +0530

    Update 35.c

--- 35.c
+++ 35.c
@@ -4,11 +4,13 @@ int main()
 char stc[20];
 int i=0,count=0;
 gets(stc);
-while( (0<=stc[i]<=9) && (stc[i]!='\0') )
+while(stc[i]!='\0')  
 {
+if(stc[i]>='0' && stc[i]<='9')	
 count++;
 i++;
 }
+
 
 printf("%d",count);
 return 0;
