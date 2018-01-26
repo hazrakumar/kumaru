db1bcef948d949881d20a91ce7b850f29a908a9c
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-26 06:48:39 -0800

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int first=0,second=1,sum,k,t,num,i;
+scanf("%d",&num);
+printf("%d\n%d\n",first,second);
+for(i=1;i<num;i++)
+{
+sum=first+second;
+first=second;
+second=sum;
+printf("%d\n",sum);
+}
+
+return 0;
+}
+
