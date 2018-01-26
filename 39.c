ca92a981f9fb6c3a29f5c8bc3d3846a7e6c00e9f
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-26 16:57:08 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i
+for(i=0;i<10;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<10;i++)
+{
+for(j=i+1;j<10;j++)
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+printf("%d",a[9]);
+return 0;
+}
