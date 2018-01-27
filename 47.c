367d4a931739e2efd28b39b198af1215e31976b7
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-01-27 07:24:26 -0800

    Update 47.c

--- 47.c
+++ 47.c
@@ -2,10 +2,26 @@
 #include<stdio.h>
 int main()
 {
-int num,i,k    ;
+int a[50],num,i,k,j    ;
 scanf("%d",&num);
 for(i=0;i<num;i++)
 { 
+  scanf("%d",&a[i]);
+}
+  for(i=0;i<num;i++)
+  {
+    for(j=i+1;j<num;j++)
+    {
+      if(a[i]>a[j])
+      {
+        k=a[i];
+        a[i]=a[j];
+        a[j]=k;
+      }
+    }
+  }
+  printf("%d\n",a[0]);
+  printf("%d",a[2]);
 return 0;
 }
 
