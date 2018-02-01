8265d87c7160bc84245864648330fdbbd3b51ebc
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-02-01 15:14:48 +0530

    Update 56.c

--- 56.c
+++ 56.c
@@ -2,7 +2,7 @@
 int main()
 {
 char stc[20];
-int i,count1=0,count2=0,count3=0,count,a=0,b=0,c=0;
+int i,count1=0,count2=0,count,a=0,b=0,c=0;
 gets(stc);
 for(i=0;stc[i]!='\0';i++)
 {
@@ -14,7 +14,10 @@ else if(stc[i]>='0' && stc[i]<='9')
 {
 count2++;
 }
-
+if((count1>0) && (count2>0))
+  printf("yes");
+  else
+    print("no");
 return 0;
 
 }
