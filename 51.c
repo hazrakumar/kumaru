2d275742bdd0e57cd99a04708b205dc502cec6a7
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-02-01 14:30:32 +0530

    Update 51.c

--- 51.c
+++ 51.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 int main()
 {
-int num,i=0,k[50],temp;
+int num,i=0,k[50],temp,count=0;
 scanf("%d",&num);
-for(i=0;num!=0;i++)
+for(i=0;num!=0;i++,count++)
 {
 temp=num%10;
 k[i]=temp;
 num=num/10;
 }
-for(i=0;k[i]!='\0';i++)
+for(i=0;i<count;i++)
 {
 printf("%d",k[i]);
 }
 return 0;
 }
+
