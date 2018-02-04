af7819c6719489fad4eff5182a71979f5c588431
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-02-04 11:51:47 +0530

    Update 61.c

--- 61.c
+++ 61.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-char s[10];
+char s[15];
 int i,k;
-gets(s);
+scanf("%s",s);
 scanf("%d",&k);
-for(i=0;i=k;i++)
+for(i=0;i<k;i++)
 {
-printf("%s",s[i]);
+printf("%c",s[i]);
 }
 return 0;
 }
