0a2607aaf8a4cb0f4c9f51bc24be76fa06a46861
hazrakumar <35258114+hazrakumar@users.noreply.github.com>
2018-02-06 14:55:29 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char s[20];
+int i;
+scanf("%d",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]>0 && s[i]<9)
+{
+printf("its a binary representation");
+break;
+}
+}
+return 0;
+}
